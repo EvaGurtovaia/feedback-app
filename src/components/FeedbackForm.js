3d5bc4324ae83e26299eee6ea552a1f725a2af85
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-27 15:14:36 -0500

    Custom button component was added

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 import "./FeedbackForm.css";
 
 const FeedbackForm = () => {
     const [newFeedback, setNewFeedback] = useState("");
 
-    const handleTextChange = e => {
-       setNewFeedback(e.target.value);
-    }
+    const handleTextChange = (e) => {
+        setNewFeedback(e.target.value);
+    };
 
     return (
         <Card>
             <form>
                 <h2>How would you rate your service with us?</h2>
                 <div className="input-group">
-                    <input  onChange={handleTextChange}type="text" placeholder="Write a review" value={newFeedback}></input>
-                    <button type="submit">Send</button>
+                    <input
+                        onChange={handleTextChange}
+                        type="text"
+                        placeholder="Write a review"
+                        value={newFeedback}
+                    ></input>
+                    <Button type="submit">Send</Button>
                 </div>
             </form>
         </Card>
